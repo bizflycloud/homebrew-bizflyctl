67f324b4226c62beb12c990ce0fcd4a69d97b282
Tùng Đỗ Sơn <svtt.tungds@vccloud.vn>
2022-03-04 08:54:17 +0700

    remove: checking hardware cond on macos

--- Formula/bizflyctl.rb
+++ Formula/bizflyctl.rb
@@ -4,13 +4,11 @@ class Bizflyctl < Formula
   version "0.2.2"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/bizflycloud/bizflyctl/releases/download/v0.2.2/bizflyctl_Darwin_x86_64.tar.gz"
-      sha256 "25669a5481c20eb9a3492ea23fea970fa5cfb829894746abab8835219d0ae01a"
+    url "https://github.com/bizflycloud/bizflyctl/releases/download/v0.2.2/bizflyctl_Darwin_x86_64.tar.gz"
+    sha256 "25669a5481c20eb9a3492ea23fea970fa5cfb829894746abab8835219d0ae01a"
 
-      def install
-        bin.install "bizfly"
-      end
+    def install
+      bin.install "bizfly"
     end
   end
 
